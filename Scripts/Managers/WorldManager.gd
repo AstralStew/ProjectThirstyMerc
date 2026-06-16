48e7a6251b62a62a6ce1f324a7a185b0bf45b0e9
AstralStew <pinky@paperticketstudios.com>
2026-06-16 12:59:22 +1000

    Starting time implementation

--- Scripts/Managers/WorldManager.gd
+++ Scripts/Managers/WorldManager.gd
@@ -19,7 +19,59 @@ static var entities_root : Node2D :
 static var effects_root : Node2D :
 	get: return instance.effects
 
+@export var day_duration: float = 120
+@export var day_start_in_mins: float = 480
+@export var day_end_in_mins: float = 480
+# total mins = 1440
 
+#@export_category("READ ONLY")
+static var is_daytime : bool = false
+
+var day_progress: float = 0 :
+	get: return day_progress
+	set(value):
+		day_progress = clamp(value,0,1)
+var day_fake_time: float = 0 :
+	get:
+		
+		return day_progress
+	set(value):
+		day_progress = clamp(value,0,1)
+
+signal _day_started
+static func day_started() -> Signal:
+	return instance._day_started
+signal _day_ended
+static func day_ended() -> Signal:
+	return instance._day_ended
+
+
+func get_fake_mins_from_progress(progress:float) -> void:
+	return remap(day_progress,0,1,day_start_in_mins,day_end_in_mins)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_daytime: 
+		progress_day(delta)
+
+func progress_day(delta: float) -> void:
+	
+	day_progress += 1 / day_duration * delta
+	
+	if day_progress >= 1:
+		end_day()
+
+static func start_day() -> void:
+	instance.day_progress = 0
+	is_daytime = true
+	day_started().emit()
+
+static func end_day() -> void:
+	instance.day_progress = 1
+	is_daytime = false
+	day_ended().emit()
 
 static func get_speed_at_tile_position(global_position:Vector2) -> float:
 	var current_tile_pos = instance.tile_map_layer.local_to_map(instance.tile_map_layer.to_local(global_position))
@@ -41,9 +93,3 @@ static func get_speed_at_tile_position(global_position:Vector2) -> float:
 		#var _polygon = (nav_region as NavigationRegion2D).navigation_polygon.get_outline()
 		#var _new_rand_point_gen = PolygonRandomPointGenerator.new()
 	#
-
-
-
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
